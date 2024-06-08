cefd7bf6284b0fa3a7ff75f82fcdd46d0cc8507b
Chandhru <64609696+Chandhru008@users.noreply.github.com>
2024-06-08 12:39:42 +0530

    if-else using input function

---
+++ Condtional Statement/if-else_using_input.py
@@ -0,0 +1,7 @@
+var =int(input("enter a number:"))
+if var > 10:
+    print("variable is greater than 10")
+elif var < 10:
+    print("variable is smaller than 10")
+else:
+    print("value is equal")
