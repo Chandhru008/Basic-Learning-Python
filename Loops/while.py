cfc5018235bce27087ed9c09701f80d2b7c3fed7
Chandhru <64609696+Chandhru008@users.noreply.github.com>
2024-06-08 13:14:24 +0530

    Initial commit

---
+++ Loops/while.py
@@ -0,0 +1,8 @@
+# while loops keeps on executing untill the condition becomes false
+var = 10
+while var<=10 and var>=1:
+    print("#{}- hello world ".format(var))
+    var=var-1 
+
+print("loop exited")    
+    
